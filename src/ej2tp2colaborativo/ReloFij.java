4ce51cac4dd85dd100e64591d26fe0d4d4ad15bf
MelinaZoeLucero <luceromelina.ml@gmail.com>
2024-08-19 17:31:19 -0300

    atributos
    nuevo atributo

--- src/ej2tp2colaborativo/ReloFij.java
+++ src/ej2tp2colaborativo/ReloFij.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej2tp2colaborativo;
 
-/**
- *
- * @author Usuario
- */
-public class ReloFij {
+
+public class ReloFij extends Reloj{
+    
+    public ReloFij(String hora, String dia, String modelo, long numSerie) {
+        super(hora, dia, modelo, numSerie);
+    }
+    
     
 }
--- src/ej2tp2colaborativo/Reloj.java
+++ src/ej2tp2colaborativo/Reloj.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej2tp2colaborativo;
 
-/**
- *
- * @author Usuario
- */
 public class Reloj {
+    private String hora, dia, modelo;
+    private long numSerie;
+
+    public Reloj(String hora, String dia, String modelo, long numSerie) {
+        this.hora = hora;
+        this.dia = dia;
+        this.modelo = modelo;
+        this.numSerie = numSerie;
+    }
+    
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public long getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(long numSerie) {
+        this.numSerie = numSerie;
+    }
+    
+    
     
 }
